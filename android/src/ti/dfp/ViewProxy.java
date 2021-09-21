281ce8a47448c3a5b09efcae6280f98befbf2322
m1ga <migamiga@web.de>
2021-09-21 17:01:42 +0200

    update dependencies, ti sdk 10 check

--- android/src/ti/dfp/ViewProxy.java
+++ android/src/ti/dfp/ViewProxy.java
@@ -1,62 +1,70 @@
 package ti.dfp;
 
+import android.app.Activity;
 import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.kroll.annotations.Kroll;
-import org.appcelerator.titanium.proxy.TiViewProxy;
 import org.appcelerator.kroll.common.Log;
+import org.appcelerator.titanium.proxy.TiViewProxy;
 import org.appcelerator.titanium.view.TiUIView;
 
-import android.app.Activity;
-
 @Kroll.proxy(creatableInModule = DfpModule.class)
-public class ViewProxy extends TiViewProxy {
+public class ViewProxy extends TiViewProxy
+{
 	private View dfpView;
-	private static final String TAG = "DfpViewProxy";
+	private static final String TAG = "ti.dfp";
 
-	public ViewProxy() {
+	public ViewProxy()
+	{
 		super();
 	}
 
 	@Override
-	protected KrollDict getLangConversionTable() {
+	protected KrollDict getLangConversionTable()
+	{
 		KrollDict table = new KrollDict();
 		table.put("title", "titleid");
 		return table;
 	}
 
 	@Override
-	public TiUIView createView(Activity activity) {
+	public TiUIView createView(Activity activity)
+	{
 		dfpView = new View(this);
 		return dfpView;
 	}
 
 	@Kroll.method
-	public void requestAd() {
-		Log.d(TAG, "[ti.dfp] requestAd()");
+	public void requestAd()
+	{
+		Log.d(TAG, "requestAd()");
 		dfpView.requestAd();
 	}
 
 	@Override
-	public void onDestroy(Activity activity) {
+	public void onDestroy(Activity activity)
+	{
 		dfpView.destroy();
 	}
 
 	@Override
-	public void onPause(Activity activity) {
+	public void onPause(Activity activity)
+	{
 		dfpView.pause();
 	}
 
 	@Override
-	public void onResume(Activity activity) {
+	public void onResume(Activity activity)
+	{
 		dfpView.resume();
 	}
 
 	@Override
-	public void onStart(Activity activity) {
+	public void onStart(Activity activity)
+	{
 	}
 
 	@Override
-	public void onStop(Activity activity) {
+	public void onStop(Activity activity)
+	{
 	}
-
 }
